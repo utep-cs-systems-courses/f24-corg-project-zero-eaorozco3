1a8e4f67f234df741c23169ed1d0518a37c8f651
eaorozco3 <eaorozco3@miners.utep.edu>
2024-09-16 10:57:22 -0600

    Completed the arrow method. Could potentially implement it more efficiently.

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,35 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+// Prints an arrow of specific length and arrowhead size.
+void print_arrow(int length, int size) {
+  int reachSize = 1;
+  int reachedHalf = 0;
+  
+  for(int i = 0; i < length; i++) {
+    // If i is half the stated length, print a full line denoting the arrow shaft.
+    if(i == length / 2) {
+      for(int j = 0; j < length; j++) {
+	putchar('*');
+      }
+    }
+
+    // Print empty space. But keep the appropriate space to print the arrowhead after.
+    else {
+      for(int j = 0; j < length; j++) {
+	putchar(' ');
+      }
+    }
+
+    // i will depend on reachSize, which will decrease or increase depending on arrowhead.
+    for(int i = 0; i < reachSize && reachSize > 0; i++) {
+      putchar('*');
+    }
+
+    // Once half of the arrowhead reached, stars are printed in decremental order.
+    reachSize == size / 2 || reachedHalf ? reachSize--, reachedHalf = 1 : reachSize++;
+    
+    putchar('\n');
+  }
+}
+
