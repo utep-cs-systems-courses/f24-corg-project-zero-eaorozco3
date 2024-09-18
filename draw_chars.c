3e1e14c025b01298d1d402221a96534bcd1f81fc
eaorozco3 <eaorozco3@miners.utep.edu>
2024-09-18 00:17:44 -0600

    Successfully implemented the 11x16 font for character printing. These changes were added on the draw_chars.c file. Appropriate changes were made on both the Makefile and draw.h to include all relevant object files and declarations. The font 8x12 will be next to be added.

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,33 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+
+void print_char_8x12(char c) {
+  c -= 0x20;
+  for(char row = 0; row < 8; row++) {
+    for(char col = 0; col < 12; col++) {
+      unsigned short rowBits = font_8x12[c][row];
+      unsigned short colMask = 1 << (11 - col);
+      putchar((rowBits & colMask) ? '*' : ' ');
+    }
+
+    putchar('\n');
+  }
+
+  putchar('\n');
+}
+
+void print_char_11x16(char c) {
+  c -= 0x20;
+  for(char row = 0; row < 11; row++) {
+    unsigned short rowBits = font_11x16[c][row];
+    for(char col = 0; col < 16; col++) {
+      unsigned short colMask = 1 << (15 - col);
+      putchar((rowBits & colMask) ? '*' : ' ');
+    }
+
+    putchar('\n');
+  }
+
+  putchar('\n');
+}
